314f81a76ec6eb8058dfc8149c90c9a3bb4520f3
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2022-10-12 19:23:22 +0800

    alhamdulillah

--- src/components/EachQuiz/EachQuiz.js
+++ src/components/EachQuiz/EachQuiz.js
@@ -2,22 +2,32 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Options from './Options/Options';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+import toast, { Toaster } from 'react-hot-toast';
 
 const EachQuiz = ({ questions }) => {
-    const { question } = questions
-    const showAns = () => {
-        console.log('answer')
+    const { question, id, options } = questions
+    console.log(options)
+    const correct = questions.correctAnswer
+
+
+    const notify = (id) => {
+        toast.success(`Correct Answer Is:  ${correct}`);
     }
+
 
     return (
         <div className='mx-auto container border-2 rounded-md my-3 py-3'>
             <div className=' flex my-3 rounded-md mx-10 py-6 justify-between '  >
-                <h2 className='text-2xl font-bold'>{question}</h2>
-                <p onClick={showAns} className='text-xl'><FontAwesomeIcon icon={faEye} /></p>
+                <h2 className='text-2xl font-bold'>Quiz:  {question.slice(3, -4)}</h2>
+                <p onClick={() => notify(id)} className='text-xl cursor-pointer'><FontAwesomeIcon icon={faEye} /></p>
             </div>
             <div className='sm:grid sm:grid-cols-2 gap-2 ml-32'>
                 {
-                    questions.options.map(option => <Options option={option}></Options>)
+                    options.map(option => <Options option={option} correct={correct} key={option.id}>
+                        <Toaster />
+                    </Options>
+
+                    )
                 }
             </div>
 
--- src/components/EachQuiz/Options/Options.js
+++ src/components/EachQuiz/Options/Options.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
-const Options = ({ option }) => {
-    const notify = () => toast.success('Option Selected!', { autoclose: 600 });
+const Options = ({ option, correct }) => {
+    console.log(correct)
+    // console.log(option)
+    const notify = () => {
+        console.log('right')
+        if (correct === option) {
+            toast.success('Your answer is Correct!! ');
+        } else {
+            toast.error('Your Answer is Wrong!')
+        }
+    }
 
 
     return (
         <div className='grid grid-cols-2'>
             <div className='flex'>
                 <div className='mr-6 font-bold'>
-                    <button onClick={notify}><input type="radio" /></button>
+                    <button onClick={notify}><input type="radio" name='nam' id='name' /></button>
                     <Toaster />
 
                 </div>
                 <div className='font-bold  '>
                     {option}
+                    <Toaster />
                 </div>
             </div>
         </div>
